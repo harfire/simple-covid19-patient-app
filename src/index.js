3bf3f4141ef67e31375b2b4c71c13c23bc92c81f
Haris Rahman <harisrahman@Hariss-MacBook-Pro.local>
2022-09-02 17:16:27 +0700

    chore: update deps

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
 import './assets/css/index.css';
 
 import Layout from './components/Layout';
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>
+);
 
 serviceWorker.unregister();
